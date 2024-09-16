3be1cad18a0626bf90b9ac64f23dd256e7ede043
Migueks <lhlmiguel@gmail.com>
2024-09-16 14:44:56 +0200

    .

---
+++ modules.js
@@ -0,0 +1,12 @@
+export const person = {
+  name: "Miguel",
+  surname: "Lopez",
+};
+
+export const sayHello = () => {
+  console.log("Hello!");
+};
+
+const DEV_LAN = "JavaScript";
+
+export default DEV_LAN;
